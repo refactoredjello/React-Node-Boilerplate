a2ac4a3434725b6b77a7615f9817bdb075e91f5d
refactoredjello <gil.mandler@gmail.com>
2018-02-07 16:02:31 -0500

    Adds css loader to webpack and import styles in index.jsx

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -2,4 +2,6 @@ import React, { Component } from 'react'
 import { render } from 'react-dom'
 import App from './App'
 
+import './styles'
+
 render(<App />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+
 
 const paths = {
   DIST: path.resolve(__dirname, 'client/dist/'),
@@ -7,29 +9,34 @@ const paths = {
   JS: path.resolve(__dirname, 'client/src/js')
 }
 
-const HtmlWebpackPluginConfig = 
-
 module.exports = {
-  entry: path.join(paths.JS, 'index.js'),
+  entry: path.join(paths.JS, 'index.jsx'),
   output: {
     path: paths.DIST,
     filename: 'bundle.js'
   },
   watch: true,
+  plugins: [
+    new HtmlWebpackPlugin({
+    template: path.join(paths.SRC, 'index.html')
+  }),
+  new ExtractTextPlugin('style.bundle.css')
+  ],
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract({
+          use: 'css-loader'
+        })
       }
     ]
   },
-  plugins: [
-      new HtmlWebpackPlugin({
-      template: path.join(paths.SRC, 'index.html')
-    })
-  ],
   resolve: {
     extensions: ['.js', '.jsx']
   }

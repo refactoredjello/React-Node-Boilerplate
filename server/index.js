d6a8da598ff5308ea58aea0a89b7cb393f6a80ff
refactoredjello <gil.mandler@gmail.com>
2018-02-11 21:07:14 -0500

    Removes body-parser in favor of express.json()

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const path = require('path')
 
 const app = express()
-app.use(bodyParser.json())
+app.use(express.json())
 app.use(express.static(path.resolve(__dirname + '/../client/dist/')))
 
 
